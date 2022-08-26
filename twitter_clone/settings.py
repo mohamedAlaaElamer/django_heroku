f94b8e6f61974c5afc2c3c2dfaa4370bf31f4760
mohamedAlaaElamer <elamermohamed440@gmail.com>
2022-08-26 07:19:58 +0200

    Added files and changes required for deployment to heroku

--- twitter_clone/settings.py
+++ twitter_clone/settings.py
@@ -204,6 +204,7 @@ REST_FRAMEWORK = {
 CORS_ALLOW_ALL_ORIGINS = True
 
 # Heroku: Update database configuration from $DATABASE_URL.
+#Heroku
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
